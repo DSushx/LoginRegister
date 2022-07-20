b4d8676c2316a8ea4358c8d26572faef24bb33c5
Power_M <power0800084@gmail.com>
2022-07-20 21:51:09 +0800

    TEST4

--- app/src/main/java/com/example/loginregister/Login.java
+++ app/src/main/java/com/example/loginregister/Login.java
@@ -63,7 +63,7 @@ public class Login extends AppCompatActivity {
                             String[] data = new String[2];
                             data[0] = username;
                             data[1] = password;
-                            PutData putData = new PutData("http://10.1.1.14/LoginRegister/login.php", "POST", field, data);
+                            PutData putData = new PutData("http://121310.1.1.14/LoginRegister/login.php", "POST", field, data);
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
                                     progressBar.setVisibility(View.GONE);
