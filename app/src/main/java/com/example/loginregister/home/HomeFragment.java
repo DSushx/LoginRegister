c1f52aa82025b586ab6d7ea42f9a02a1ad2ad959
yujiaan <a1085538@mail.nuk.edu.tw>
2022-11-13 19:57:20 +0800

    測試下

--- app/src/main/java/com/example/loginregister/home/HomeFragment.java
+++ app/src/main/java/com/example/loginregister/home/HomeFragment.java
@@ -3,4 +3,5 @@ package com.example.loginregister.home;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+
 }
--- app/src/main/java/com/example/loginregister/home/test.java
+++ app/src/main/java/com/example/loginregister/home/test.java
@@ -1,5 +1,5 @@
 package com.example.loginregister.home;
 
 public class test {
-    //
+    ////
 }
