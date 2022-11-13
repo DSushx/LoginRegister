b577e794e242cd9bf7a19ac7bf3d3bb856170565
Bubu <a0910499399@gmail.com>
2022-11-13 19:49:45 +0800

    okok

--- app/src/main/java/com/example/loginregister/Login.java
+++ app/src/main/java/com/example/loginregister/Login.java
@@ -64,7 +64,7 @@ public class Login extends AppCompatActivity {
                             String[] data = new String[2];
                             data[0] = username;
                             data[1] = password;
-                            PutData putData = new PutData("http://192.168.100.35/LoginRegister/login.php", "POST", field, data);
+                            PutData putData = new PutData("http://192.168.56.1/LoginRegister/login.php", "POST", field, data);
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
                                     progressBar.setVisibility(View.GONE);
