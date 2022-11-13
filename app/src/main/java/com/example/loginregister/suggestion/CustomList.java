50799adf70ddfb9f694ba0082b6a882398773285
PeiWenFu <a1077160@mail.nuk.edu.tw>
2022-11-13 18:38:22 +0800

    推薦介面

---
+++ app/src/main/java/com/example/loginregister/suggestion/CustomList.java
@@ -0,0 +1,4 @@
+package com.example.loginregister.suggestion;
+
+public class CustomList {
+}
---
+++ app/src/main/java/com/example/loginregister/suggestion/FoodInfo.java
@@ -0,0 +1,15 @@
+package com.example.loginregister.suggestion;
+
+public class FoodInfo {
+    Integer food_id;
+    String title;
+    String categories;
+    String tags;
+    Double weight;
+    Double calories;
+    Double protein;
+    Double carbs;
+    Double fat;
+    Double sugar;
+    Double sodium;
+}
